9926bb12f2e1cdffb057ed217c39e4cd5dead97a
Arthur Goulart <arthurgoulartexec@gmail.com>
2021-08-12 16:24:09 -0300

    Habitos Card

---
+++ src/components/HabitosCard/PurpleCard/index.jsx
@@ -0,0 +1,39 @@
+import { HabitCard } from "./styles";
+
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
+
+const PurpleCard = () => {
+  return (
+    <>
+      <HabitCard>
+        <div className="filled">
+          <div />
+        </div>
+        <div className="text">
+          <div>titulo</div>
+          <div>Descrição</div>
+          <div>
+            Nivel:
+            <SettingsOutlinedIcon />
+            <SettingsOutlinedIcon />
+          </div>
+        </div>
+        <div className="text2">
+          <div className="pen">
+            <CreateOutlinedIcon />
+          </div>
+          <button className="btn">Categoria</button>
+          <div>Frenquencia:</div>
+        </div>
+
+        <div className="filled2">
+          <button class="plus">+</button>
+          <button class="minus">-</button>
+        </div>
+      </HabitCard>
+    </>
+  );
+};
+
+export default PurpleCard;
---
+++ src/components/HabitosCard/RedCard/styles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const HabitCard = styled.div`
+  border: 1px solid black;
+  display: flex;
+  height: 100px;
+  width: 45%;
+  justify-content: space-between;
+  .filled {
+    background-color: #f8bbd0;
+    width: 16%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    div {
+      border: 3px solid #f50057;
+      background-color: #f8bbd0;
+      border-radius: 100%;
+      padding: 31px;
+    }
+  }
+
+  .filled2 {
+    background-color: #f8bbd0;
+    width: 16%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+
+    .plus {
+      border: 1px solid #000000;
+      width: 30%;
+      height: 33%;
+      border-radius: 88%;
+    }
+
+    .minus {
+      border: 1px solid #000000;
+
+      width: 30%;
+      height: 33%;
+      border-radius: 88%;
+    }
+  }
+
+  .text {
+    display: flex;
+    justify-content: space-evenly;
+
+    flex-direction: column;
+    width: 46%;
+  }
+
+  .text2 {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    .pen {
+      text-align: end;
+    }
+    .btn {
+      background-color: #f8bbd0;
+      border-radius: 5px;
+      border: none;
+      opacity: 50%;
+    }
+  }
+`;
