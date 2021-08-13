d3cfd3d2c9d9e36cecec5217755125eddf04c763
Felipe Nunes <luis.felipe.nb@hotmail.com>
2021-08-13 15:52:14 -0300

    feat(login): login page complete

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Login from '../pages/Login';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route path='/login'>
+        <Login/>
       </Route>
       <Route path="/register">
         <Register />
