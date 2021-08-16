bd694a8e022942541dbcfefc3d47ed14aa915fda
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-08-16 18:02:05 -0300

    style(menuside): increasing the size of the fonts and changing the font-family to "Arvo"

--- src/components/MenuSide/index.jsx
+++ src/components/MenuSide/index.jsx
@@ -1,15 +1,10 @@
-import { MenuBar, TitleBar, ContainerPai } from "./styles";
-
+import { MenuBar, TitleBar, Container } from "./styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
-
 import LineStyleIcon from "@material-ui/icons/LineStyle";
-
 import GroupIcon from "@material-ui/icons/Group";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
 import { useState } from "react";
 
 const MenuSide = () => {
@@ -32,12 +27,12 @@ const MenuSide = () => {
 
   return (
     <>
-      <ContainerPai>
+      <Container>
         <TitleBar>
           <div className="title">Proscrastinare</div>
         </TitleBar>
         <MenuBar>
-          <div className="pages">
+          <section className="pages">
             <div className="option">
               <MenuIcon />
               <div className="text">dashboard</div>
@@ -50,35 +45,23 @@ const MenuSide = () => {
               <LineStyleIcon />
               <div className="text">hábitos</div>
             </div>
-            <div className="group">
+            <div className="group" onClick={ShowGroups}>
               <GroupIcon />
-              <div
-                className="main-groups"
-                onClick={() => {
-                  ShowGroups();
-                }}
-              >
-                Grupos
-              </div>
-              <ExpandMoreIcon
-                className="down-arrow"
-                onClick={() => {
-                  ShowGroups();
-                }}
-              />
+              <div className="main-groups">grupos</div>
+              <ExpandMoreIcon className="down-arrow" />
             </div>
             <div className={showDiv}>
               <div className={appear}>meus grupos</div>
               <div className={appear}>encontrar grupos</div>
               <div className={appear}>criar novo grupo</div>
             </div>
-          </div>
+          </section>
           <div className="logout">
             <ExitToAppIcon />
             <div>logout</div>
           </div>
         </MenuBar>
-      </ContainerPai>
+      </Container>
     </>
   );
 };
