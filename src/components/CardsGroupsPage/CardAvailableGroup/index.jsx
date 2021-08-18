3e66f7d4ba8f6e5b6f975945541b5d658fd69f50
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-08-18 12:33:32 -0300

    feat(cardavailablegroup): adding conditionals renders related to group category
    n

--- src/components/CardsGroupsPage/CardAvailableGroup/index.jsx
+++ src/components/CardsGroupsPage/CardAvailableGroup/index.jsx
@@ -1,38 +1,144 @@
 import { Container, Icon } from "./styles";
-
 import ControlPointIcon from "@material-ui/icons/ControlPoint";
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import NaturePeopleIcon from '@material-ui/icons/NaturePeople'; 
 
 const CardAvailableGroup = ({name, description, category}) => {
-  return (
-      <Container color="#E57373">
-      <div className="card_colored-part" />
-        <div className="card_content">
 
-          <div className="card_header">
-              <Icon></Icon>
-              <div className="group">
-                  <h3>{name}</h3>
-                  <span>{category}</span>
+  if(category === "Saúde") {
+    return (
+        <Container color="#E57373">
+          <div className="card_colored-part" />
+            <div className="card_content">
+
+              <div className="card_header">
+                  <Icon><FitnessCenterIcon /></Icon>
+                  <div className="group">
+                      <h3>{name}</h3>
+                      <span>{category}</span>
+                  </div>
               </div>
-          </div>
 
-          <div className="card_description">
-              <p><span>descrição: </span>
-              {description}
-              </p>
-          </div>
+              <div className="card_description">
+                  <p><span>descrição: </span>
+                  {description}
+                  </p>
+              </div>
 
-          <div className="card_actions">
-            <button>
-                <ControlPointIcon />
-                <span>inscrever</span>
-            </button>
-            <span className="seeMore">ver mais</span>
-          </div>
+              <div className="card_actions">
+                <button>
+                    <ControlPointIcon />
+                    <span>inscrever</span>
+                </button>
+                <span className="seeMore">ver mais</span>
+              </div>
 
-        </div>
-        <div className="card_colored-part" />
-      </Container>   
+            </div>
+            <div className="card_colored-part" />
+          </Container>   
+      )
+  }
+  
+  if(category === "Alimentação") {
+    return (
+        <Container color="#7986CB">
+          <div className="card_colored-part" />
+            <div className="card_content">
+
+              <div className="card_header">
+                  <Icon><FastfoodIcon /></Icon>
+                  <div className="group">
+                      <h3>{name}</h3>
+                      <span>{category}</span>
+                  </div>
+              </div>
+
+              <div className="card_description">
+                  <p><span>descrição: </span>
+                  {description}
+                  </p>
+              </div>
+
+              <div className="card_actions">
+                <button>
+                    <ControlPointIcon />
+                    <span>inscrever</span>
+                </button>
+                <span className="seeMore">ver mais</span>
+              </div>
+
+            </div>
+            <div className="card_colored-part" />
+          </Container>   
+      )
+  }
+  
+  if(category === "Estudo") {
+    return (
+        <Container color="#81C7843">
+          <div className="card_colored-part" />
+            <div className="card_content">
+
+              <div className="card_header">
+                  <Icon><MenuBookIcon /></Icon>
+                  <div className="group">
+                      <h3>{name}</h3>
+                      <span>{category}</span>
+                  </div>
+              </div>
+
+              <div className="card_description">
+                  <p><span>descrição: </span>
+                  {description}
+                  </p>
+              </div>
+
+              <div className="card_actions">
+                <button>
+                    <ControlPointIcon />
+                    <span>inscrever</span>
+                </button>
+                <span className="seeMore">ver mais</span>
+              </div>
+
+            </div>
+            <div className="card_colored-part" />
+          </Container>   
+      )
+  }
+
+  return (
+    <Container color="#26A69A">
+          <div className="card_colored-part" />
+            <div className="card_content">
+
+              <div className="card_header">
+                  <Icon><NaturePeopleIcon /></Icon>
+                  <div className="group">
+                      <h3>{name}</h3>
+                      <span>{category}</span>
+                  </div>
+              </div>
+
+              <div className="card_description">
+                  <p><span>descrição: </span>
+                  {description}
+                  </p>
+              </div>
+
+              <div className="card_actions">
+                <button>
+                    <ControlPointIcon />
+                    <span>inscrever</span>
+                </button>
+                <span className="seeMore">ver mais</span>
+              </div>
+
+            </div>
+            <div className="card_colored-part" />
+          </Container>  
   );
 };
 
